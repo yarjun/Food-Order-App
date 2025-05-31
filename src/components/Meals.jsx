a2b3fad0bb8ce0416bf49aebc99dc9aa361d6a7d
yarjun <arjunyadav15597@gmail.com>
2025-05-31 00:18:57 -0400

    Updates

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -9,7 +9,7 @@ export default function Meals() {
         data: loadedMeals,
         isLoading,
         error
-    } = useHttp('http://localhost:3000/meals',requestConfig, []);
+    } = useHttp('https://food-order-app-w7np.onrender.com/meals',requestConfig, []);
 
     if(isLoading){
         return <p className='center'>Fetching meals...</p>;
